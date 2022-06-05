d25a7504a4cef00174438439c21eac3b8392346a
Oyefesojoshua2 <104689282+Oyefesojoshua2@users.noreply.github.com>
2022-06-05 11:51:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function sst(){
+    alert("welcome to the secret page and you must answer the question to continue")
+  let a=prompt("input password")
+    if(a == "seesecretpagenow"){
+      alert(" correct")
+      window.location="sp.html"
+    }else{
+      alert("an error occurred,😂😂😂 you think am so stupid to add"+'"'+a+'"'+" as my password well nice try "+ a+" is a very wrong password try again")
+      console.warn("an error occurred,😂😂😂 you think am so stupid to add"+'"'+a+'"'+" as my password well nice try "+ a+" is a very wrong password try again")
+      console.error("ERROR INTRUDER ALERT YOUR INFORMATION WILL BE SENT TO THE PRODUCER FOR TRYING TO BREAK IS PRIVACY SECTION AND BE READY TO FACE THE REPERCAUSION")
+    }
+
+
+  }
+console.log("HELLO AND WELCOME TO MY PAGE")
+console.log("AS YOU KNOW MY PORTFOLIO")
+console.warn("PLEASE THIS WEBSITE SHOULD NOT BE PIRATED IF YOU ARE CAUGHT YOU WILL HAVE TO FACE THE CONSEQUENCE")
+
